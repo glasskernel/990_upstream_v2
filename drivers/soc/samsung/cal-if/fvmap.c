76fecb540b2b1aa0a5869f1d60fb07116efea163
snapdragon teaks <inutilidades639@gmail.com>
2025-11-20 14:58:32 -0300

    Revert "drivers: soc: cal-if: Prevent optimization of structs on fvmap_copy_from_sram"
    This reverts commit cfc6d997f6a65cb3c03efb99b0472d8959220d5e.

--- drivers/soc/samsung/cal-if/fvmap.c
+++ drivers/soc/samsung/cal-if/fvmap.c
@@ -654,7 +654,7 @@ static inline ssize_t print_fvmap(char *buf, int start, int end)
 
 static void fvmap_copy_from_sram(void __iomem *map_base, void __iomem *sram_base)
 {
-	volatile struct fvmap_header *fvmap_header, *header;
+	struct fvmap_header *fvmap_header, *header;
 	struct rate_volt_header *old, *new;
 	struct clocks *clks;
 	struct pll_header *plls;
